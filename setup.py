18f59c4f0298a9ca9f3d8478d8171fa817e1f21e
BaBaBags <ryan@ryanbagwell.com>
2013-06-30 18:35:01 -0500

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "insteonic",
-    version = "0.2",
+    version = "0.3",
     author = "Ryan Bagwell",
     author_email = "ryan@ryanbagwell.com",
     description = ("A Python command-line interface for Insteon's smartlinc controller"),
